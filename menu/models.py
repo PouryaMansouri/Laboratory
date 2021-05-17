5f57735bb5dac1019f3f00536f1dba11095fcd5e
Pourya Mansouri <pourya.mansouri@hotmail.com>
2021-05-17 14:31:43 +0430

    create Node class and base Menu

--- menu/models.py
+++ menu/models.py
@@ -1,1 +1,32 @@
+from abc import ABC
+
+from core.models import BaseModel
+
+
+class Node:
+    def __init__(self, parent=None) -> None:
+        self.children = []
+        assert parent is Node or isinstance(parent, Node)  # parent must be node
+        self.parent = parent
+        if parent:
+            self.parent.children.append(self)
+
+
+class Menu(ABC, Node):
+    def __init__(self, name, parent=None, description=""):
+        Node.__init__(self, parent)  # TODO: use super
+        self.name = name
+        self.description = description
+
+    def __repr__(self):
+        return f"This is a {self.name}  menu \n{self.description}"
+
+
+class MenuList(Menu):
+    def __call__(self, *args, **kwargs):
+        print(f'{self.parent.name} > {self.name}' if self.parent else self.name)
+        print(f'{self.description}')
+        print("\nMenu items:")
+        for id_menu, sub_menu in enumerate(self.children):
+            print(f"\t{id_menu}- {repr(sub_menu)}")
 
