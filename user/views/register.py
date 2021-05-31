66f32f6f814e374af6ff46eeae935622be17872f
hamidrezafkri <h.f.f.z.123456789@gmail.com>
2021-05-31 15:21:21 +0430

    login and register

---
+++ user/views/register.py
@@ -0,0 +1,14 @@
+def register():
+
+    global patient
+    if LOGIN==False:
+        fname=input('enter the first name :')
+        lname=input('enter the last name :')
+        phone=input('enter the phone number :')
+        #validators
+        with open('../Patient.pkl','rb') as f:
+            unpickle=pickle.load(f)
+        for i in unpickle:
+            if i.first_name==fname:
+                assert "this username exist"
+    return
