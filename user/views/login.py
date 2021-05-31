de148f77c1b3c592524bd4c88fc5e9dea7a8af38
pourya <pourya.mansouri@hotmail.com>
2021-05-31 17:47:58 +0430

    change url

--- user/views/login.py
+++ user/views/login.py
@@ -3,15 +3,16 @@ import pickle
 from user.models import Patient
 from typing import List
 
+
 def login():
     global LOGIN
-    URL=""
+    URL = "user/Patient.pkl"
     if LOGIN == True:
         print('you have already logged in')
     else:
         firstname = input("please enter your username :")
         password = input("please enter your phone number :")
-        with open('user/Patient.pkl', 'rb') as f:
+        with open(URL, 'rb') as f:
             unpickle = pickle.load(f)
         for i in unpickle:
             if i.first_name == firstname:
